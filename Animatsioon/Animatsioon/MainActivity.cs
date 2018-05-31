11c863c87e6139db5980b1b8b93c571ba7a4d691
OliverKilk <oliver.kilk@tptlive.ee>
2018-05-31 08:05:12 +0300

    code

---
+++ Animatsioon/Animatsioon/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Graphics.Drawables;
+
+namespace Animatsioon
+{
+    [Activity(Label = "Animation", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+        }
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            if (hasFocus)
+            {
+                ImageView imageView = FindViewById<ImageView>(Resource.Id.animated_android);
+                AnimationDrawable animation = (AnimationDrawable)imageView.Drawable;
+                animation.Start();
+            }
+        }
+    }
+}
+
